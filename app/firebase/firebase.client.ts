9d90e324bc857612fca84947f01fa66939480c5c
elie309 <elie.saade432@gmail.com>
2023-02-20 22:38:52 +0200

    .

--- app/firebase/firebase.client.ts
+++ app/firebase/firebase.client.ts
@@ -12,19 +12,9 @@ import { getAuth, inMemoryPersistence, setPersistence } from "firebase/auth";
 //   measurementId: process.env.FIREBASE_MEASUREMENT_ID
 // };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDVXGIN-ohPDWhgwcOj9qOJXpeNYseEfpU",
-  authDomain: "easierlife-elie309.firebaseapp.com",
-  databaseURL: "https://easierlife-elie309-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "easierlife-elie309",
-  storageBucket: "easierlife-elie309.appspot.com",
-  messagingSenderId: "1029054175448",
-  appId: "1:1029054175448:web:8b4f1ffe9e9ea795005fad",
-  measurementId: "G-0SZ269R92Z"
-};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(require("./firebase.config").firebaseConfig);
 
 const auth = getAuth(app);
 
